bf4529ddd9f1b5e0bb3b91c2ef9bf53345e81162
pigeons56 <lowpolypigeons@gmail.com>
2024-11-18 13:19:04 -0500

    Finished read from soil moisture; need to add backup watering

--- main/main.ino
+++ main/main.ino
@@ -7,3 +7,12 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+int needsWater(){
+  int value = analogRead(sensorPin);
+  if(value < 300)
+    return 1;
+  else
+    return 0;
+  //backup water if not watered in 48 hours? need a way to track time
+}
