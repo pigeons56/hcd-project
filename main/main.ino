a752710438e10e58c726d1e5bee3959c7d1e39f2
pigeons56 <lowpolypigeons@gmail.com>
2024-11-18 21:17:19 -0500

    Finish readFloatSwitch

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,28 @@
+const int floatSwitchPin = 2;
+const int ledPin = 3;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(floatSwitchPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+/*
+ * Reads the state of the float switch. If the float switch is
+ * LOW (water level is high), turn off LED. If the float switch
+ * is HIGH (water level is low), turn on LED.
+*/
+void readFloatSwitch() {
+  static bool floatSwitchState = digitalRead(floatSwitchPin);
+
+  if (floatSwitchState == LOW) {
+    digitalWrite(ledPin, LOW);
+  } else {
+    digitalWrite(ledPin, HIGH);
+  }
 }
