de19ea6f83b9848f1fc4919e5861cbd85f0fac78
pigeons56 <lowpolypigeons@gmail.com>
2024-11-18 20:43:42 -0500

    Finish waterPlant. Open pump based on needsWater.

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,38 @@
+#define PUMP_OPEN_COUNTS 1000
+
+const int pumpPin = 1;
+
+void waterPlant();
+
 void setup() {
-  // put your setup code here, to run once:
+  //Water pump setup
+  pinMode(pumpPin, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  waterPlant();
+
+}
+
+void waterPlant() {
+  static bool pumpOpen = 0;
+  static int count = 0;
+
+  if (pumpOpen) {
+    count++; //keep counting
+
+    if (count > PUMP_OPEN_COUNTS) { //after pump is open for a certain duration
+      count = 0; //reset counting
+      pumpOpen = 0; //close pump
+      digitalWrite(pumpPin, LOW); //physically close pump
+    }
+  } else {
+      if (needsWater()) { //if water conditions are met
+        pumpOpen = 1; //open pump
+        digitalWrite(pumpPin, HIGH); //physically open pump
+      }
+  }
 
 }
